f138202eda07b86abd4e633ce116828007db1e62
backmanindustries <backmanindustries@gmail.com>
2026-05-04 15:08:04 -0700

    fix CORS for production

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://buyerradar-production.up.railway.app',
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
